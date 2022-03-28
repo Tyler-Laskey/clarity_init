12e50f87254a23aab6dca3e052df13537112c8b1
Tyler Laskey <90269666+Tyler-Laskey@users.noreply.github.com>
2022-03-28 14:13:08 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -42,122 +42,143 @@ initSystemd()
   fi
 }
 
+initKeyrings()
+{
+  SECTION="docker"
+  addToLogDt "Checking ${SECTION} keyring" y
+
+  SRC_URL="https://download.docker.com/linux/ubuntu/gpg"
+  KEYRING="/usr/share/keyrings/docker-archive-keyring.gpg"
+  ST_FILE="staging/docker-gpg"
+  LST="/etc/apt/sources.list.d/docker.list"
+
+  # import docker key
+  if [ -e ${KEYRING} ]; then
+    addToLogDt "-${SECTION} keyring exists" y
+  else
+    addToLogDt "-${SECTION} keyring missing"
+    curl -fsSL ${SRC_URL} -o ${ST_FILE}|& tee -a .log
+    cat ${ST_FILE} | gpg --dearmor -o ${KEYRING} |& tee -a .log
+    if ! [ -e ${KEYRING} ]; then
+      echo "-Unable to pull ${SECTION} keyring"
+      addToLogDt "-!!! Unable to load ${SECTION} keyring !!!"
+      exit
+    else
+      addToLogDt "-${SECTION} keyring pulled successfully" y
+    fi
+  fi
+  addToLogDt "Checking ${SECTION} source" y
+  if [ -e ${LST} ]; then
+    addToLogDt "-${SECTION} source exists" y
+  else
+    addToLogDt "-Adding ${SECTION} source" y
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=${KEYRING}] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list |& tee -a .log
+  fi
+  addToLogDt "${SECTION} complete" y
 
 
+  SECTION="kubernetes"
+  addToLogDt "Checking ${SECTION} keyring" y
+
+  SRC_URL="https://packages.cloud.google.com/apt/doc/apt-key.gpg"
+  KEYRING="/usr/share/keyrings/kubernetes-archive-keyring.gpg"
+  ST_FILE="staging/kubernetes-gpg"
+  LST="/etc/apt/sources.list.d/kubernetes.list"
+
+  # import Kubernetes key
+  if [ -e ${KEYRING} ]; then
+    addToLogDt "-${SECTION} keyring exists" y
+  else
+    addToLogDt "-${SECTION} keyring missing" y
+    curl -fsSL ${SRC_URL} -o ${ST_FILE} |& tee -a .log
+    cat ${ST_FILE} | gpg --dearmor -o ${KEYRING} |& tee -a .log
+    if ! [ -e ${KEYRING} ]; then
+      echo "-Unable to pull ${SECTION} keyring"
+      addToLogDt "-!!! Unable to load ${SECTION} keyring !!!"
+      exit
+    else
+      addToLogDt "-${SECTION} keyring pulled successfully" y
+    fi
+  fi
+  addToLogDt "Checking ${SECTION} source" y
+  if [ -e ${LST} ]; then
+    addToLogDt "-${SECTION} source exists" y
+  else
+    addToLogDt "-Adding ${SECTION} source" y
+    echo "deb [signed-by=${KEYRING}] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee ${LST} |& tee -a .log
+  fi
+  addToLogDt "${SECTION} complete" y
 
 
-addToLogDt "Initializing Clarity development environment" e
+  SECTION="Helm"
+  addToLogDt "Checking ${SECTION} keyring" y
+
+  SRC_URL="https://baltocdn.com/helm/signing.asc"
+  KEYRING="/usr/share/keyrings/kubernetes-archive-keyring.gpg"
+  ST_FILE="staging/helm_signing.asc"
+  LST="/etc/apt/sources.list.d/helm-stable-debian.list"
+  if [ -e ${KEYRING} ]; then
+    addToLogDt "-${SECTION} keyring exists" y
+  else
+    addToLogDt "-${SECTION} keyring missing" y
+    curl -fsSL ${SRC_URL} -o ${ST_FILE} |& tee -a .log
+    apt-key add ${ST_FILE}
+    if ! [ -e ${KEYRING} ]; then
+      echo "-Unable to pull ${SECTION} keyring"
+      addToLogDt "-!!! Unable to load ${SECTION} keyring !!!"
+      exit
+    else
+      addToLogDt "-${SECTION} keyring pulled successfully" y
+    fi
+  fi
+  addToLogDt "Checking ${SECTION} source" y
+  if [ -e ${LST} ]; then
+    addToLogDt "-${SECTION} source exists" y
+  else
+    addToLogDt "-Adding ${SECTION} source" y
+    echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee ${LST} |& tee -a .log
+  fi
+  addToLogDt "${SECTION} complete" y
+}
+
+installPackages()
+{
+  addToLogDt "Installing required apps" y
+  apt install -y apt-transport-https ca-certificates conntrack docker-ce docker-ce-cli containerd containerd.io python3 python3-pip python3-venv kafkacat jq unzip libxcb1-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev ca-certificates curl gnupg lsb-release apt-transport-https kubectl helm 
+}
+
+installMinikube()
+{
+  addToLogDt "Installing minikube" y
+  wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -o staging/minikube
+  cp minikube /usr/local/bin/minikube
+  chmod +x /usr/local/bin/minikube
+
+}
+
+
+addToLogDt "Initializing Clarity development environment" y
 
 
 
 mkdir -p staging
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   addToLogDt "Script must be run as root"
+   addToLogDt "This script must be run as root" y
    exit 1
 fi
 
+
 initSystemd
+initKeyrings
 
-SECTION="docker"
-addToLogDt "Checking ${SECTION} keyring" y
-
-SRC_URL="https://download.docker.com/linux/ubuntu/gpg"
-KEYRING="/usr/share/keyrings/docker-archive-keyring.gpg"
-ST_FILE="staging/docker-gpg"
-LST="/etc/apt/sources.list.d/docker.list"
-
-# import docker key
-if [ -e ${KEYRING} ]; then
-  addToLogDt "-${SECTION} keyring exists" y
-else
-  addToLogDt "-${SECTION} keyring missing"
-  curl -fsSL ${SRC_URL} -o ${ST_FILE}|& tee -a .log
-  cat ${ST_FILE} | gpg --dearmor -o ${KEYRING} |& tee -a .log
-  if ! [ -e ${KEYRING} ]; then
-    echo "-Unable to pull ${SECTION} keyring"
-    addToLogDt "-!!! Unable to load ${SECTION} keyring !!!"
-    exit
-  else
-    addToLogDt "-${SECTION} keyring pulled successfully" y
-  fi
-fi
-addToLogDt "Checking ${SECTION} source" y
-if [ -e ${LST} ]; then
-  addToLogDt "-${SECTION} source exists" y
-else
-  addToLogDt "-Adding ${SECTION} source" y
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=${KEYRING}] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list |& tee -a .log
-fi
-echo "${SECTION} complete"
+installPackages
+installMinikube
 
 
-SECTION="kubernetes"
-addToLogDt "Checking ${SECTION} keyring" y
 
-SRC_URL="https://packages.cloud.google.com/apt/doc/apt-key.gpg"
-KEYRING="/usr/share/keyrings/kubernetes-archive-keyring.gpg"
-ST_FILE="staging/kubernetes-gpg"
-LST="/etc/apt/sources.list.d/kubernetes.list"
-
-# import Kubernetes key
-if [ -e ${KEYRING} ]; then
-  addToLogDt "-${SECTION} keyring exists" y
-else
-  addToLogDt "-${SECTION} keyring missing" y
-  curl -fsSL ${SRC_URL} -o ${ST_FILE} |& tee -a .log
-  cat ${ST_FILE} | gpg --dearmor -o ${KEYRING} |& tee -a .log
-  if ! [ -e ${KEYRING} ]; then
-    echo "-Unable to pull ${SECTION} keyring"
-    addToLogDt "-!!! Unable to load ${SECTION} keyring !!!"
-    exit
-  else
-    addToLogDt "-${SECTION} keyring pulled successfully" y
-  fi
-fi
-addToLogDt "Checking ${SECTION} source" y
-if [ -e ${LST} ]; then
-  addToLogDt "-${SECTION} source exists" y
-else
-  addToLogDt "-Adding ${SECTION} source" y
-  echo "deb [signed-by=${KEYRING}] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee ${LST} |& tee -a .log
-fi
-echo "${SECTION} complete"
-
-
-SECTION="Helm"
-addToLogDt "Checking ${SECTION} keyring" y
-
-SRC_URL="https://baltocdn.com/helm/signing.asc"
-KEYRING="/usr/share/keyrings/kubernetes-archive-keyring.gpg"
-ST_FILE="staging/helm_signing.asc"
-LST="/etc/apt/sources.list.d/helm-stable-debian.list"
-
-# import Kubernetes key
-if [ -e ${KEYRING} ]; then
-  addToLogDt "-${SECTION} keyring exists" y
-else
-  addToLogDt "-${SECTION} keyring missing" y
-  curl -fsSL ${SRC_URL} -o ${ST_FILE} |& tee -a .log
-  apt-key add ${ST_FILE}
-  if ! [ -e ${KEYRING} ]; then
-    echo "-Unable to pull ${SECTION} keyring"
-    addToLogDt "-!!! Unable to load ${SECTION} keyring !!!"
-    exit
-  else
-    addToLogDt "-${SECTION} keyring pulled successfully" y
-  fi
-fi
-addToLogDt "Checking ${SECTION} source" y
-if [ -e ${LST} ]; then
-  addToLogDt "-${SECTION} source exists" y
-else
-  addToLogDt "-Adding ${SECTION} source" y
-  echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee ${LST} |& tee -a .log
-fi
-echo "${SECTION} complete"
-
+addToLogDt "Initialization complete!!!" y
+exit
 
 
 
@@ -170,6 +191,11 @@ echo "${SECTION} complete"
 # run cleanup to remove staging folder
 cleanUp
 exit 1
+
+
+# This command will add the current username to the sudoers file
+# printf "${USER} ALL=(ALL) NOPASSWD: /usr/bin/dockerd\n" >> /etc/sudoers
+
 
 
 
