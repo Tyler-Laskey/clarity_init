0d938e2bf8e12a0fdc46189e6197e107ed598087
Tyler Laskey <tyler.laskey@telus.com>
2022-03-28 10:39:19 -0600

    +systemd init +kubernetes +helm

--- init.sh
+++ init.sh
@@ -22,8 +22,31 @@ addToLogDt()
   addToLog "$(date "+%y/%m/%d %H:%M:%S") | ${MSG}"
 }
 
-addToLogDt "Initializing Clarity development environment" 1
-exit 2
+cleanUp()
+{
+  rm -r staging/
+}
+
+initSystemd()
+{
+  addToLogDt "Ensuring systemd is running before continuing" y
+  if [ -z ps aux | grep -v grep | grep systemd ]; then
+    addToLogDt "- systemd is not running. Begining install..." y
+    git clone https://github.com/Tyler-Laskey/ubuntu-wsl2-systemd-script.git ~/ubuntu-wsl2-systemd-script
+    cd ~/ubuntu-wsl2-systemd-script
+    bash install.sh
+    addToLogDt "- Please quit ubuntu and restart. Once complete run this init.sh script again."
+  fi
+}
+
+
+
+
+
+addToLogDt "Initializing Clarity development environment" e
+
+
+
 mkdir -p staging
 
 if [[ $EUID -ne 0 ]]; then
@@ -32,67 +55,125 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+
 SECTION="docker"
-echo "Importing ${SECTION} keyring"
-addToLogDt "Checking ${SECTION} keyring"
+addToLogDt "Checking ${SECTION} keyring" y
+
+SRC_URL="https://download.docker.com/linux/ubuntu/gpg"
+KEYRING="/usr/share/keyrings/docker-archive-keyring.gpg"
+ST_FILE="staging/docker-gpg"
+LST="/etc/apt/sources.list.d/docker.list"
+
 # import docker key
-if [ -e /usr/share/keyrings/docker-archive-keyring.gpg ]; then
-  echo "-${SECTION} keyring exists"
-  addToLogDt "-${SECTION} keyring exists"
+if [ -e ${KEYRING} ]; then
+  addToLogDt "-${SECTION} keyring exists" y
 else
   addToLogDt "-${SECTION} keyring missing"
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o staging/ubuntu-gpg |& tee -a .log
-  cat staging/ubuntu-gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg |& tee -a .log
-  if ! [ -e /usr/share/keyrings/docker-archive-keyring.gpg ]; then
+  curl -fsSL ${SRC_URL} -o ${ST_FILE}|& tee -a .log
+  cat ${ST_FILE} | gpg --dearmor -o ${KEYRING} |& tee -a .log
+  if ! [ -e ${KEYRING} ]; then
     echo "-Unable to pull ${SECTION} keyring"
     addToLogDt "-!!! Unable to load ${SECTION} keyring !!!"
     exit
   else
-    echo "-${SECTION} keyring pulled successfully"
-    addToLogDt "-${SECTION} keyring pulled successfully"
+    addToLogDt "-${SECTION} keyring pulled successfully" y
   fi
 fi
-echo "Checking ${SECTION} source"
-addToLogDt "Checking ${SECTION} source"
-if [ -e /etc/apt/sources.list.d/docker.list ]; then
-  echo "-${SECTION} source exists"
-  addToLogDt "-${SECTION} source exists"
+addToLogDt "Checking ${SECTION} source" y
+if [ -e ${LST} ]; then
+  addToLogDt "-${SECTION} source exists" y
 else
-  echo "-Adding ${SECTION} source"
-  addToLogDt "-Adding ${SECTION} source"
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  addToLogDt "-Adding ${SECTION} source" y
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=${KEYRING}] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list |& tee -a .log
 fi
+echo "${SECTION} complete"
+
+
+SECTION="kubernetes"
+addToLogDt "Checking ${SECTION} keyring" y
+
+SRC_URL="https://packages.cloud.google.com/apt/doc/apt-key.gpg"
+KEYRING="/usr/share/keyrings/kubernetes-archive-keyring.gpg"
+ST_FILE="staging/kubernetes-gpg"
+LST="/etc/apt/sources.list.d/kubernetes.list"
+
+# import Kubernetes key
+if [ -e ${KEYRING} ]; then
+  addToLogDt "-${SECTION} keyring exists" y
+else
+  addToLogDt "-${SECTION} keyring missing" y
+  curl -fsSL ${SRC_URL} -o ${ST_FILE} |& tee -a .log
+  cat ${ST_FILE} | gpg --dearmor -o ${KEYRING} |& tee -a .log
+  if ! [ -e ${KEYRING} ]; then
+    echo "-Unable to pull ${SECTION} keyring"
+    addToLogDt "-!!! Unable to load ${SECTION} keyring !!!"
+    exit
+  else
+    addToLogDt "-${SECTION} keyring pulled successfully" y
+  fi
+fi
+addToLogDt "Checking ${SECTION} source" y
+if [ -e ${LST} ]; then
+  addToLogDt "-${SECTION} source exists" y
+else
+  addToLogDt "-Adding ${SECTION} source" y
+  echo "deb [signed-by=${KEYRING}] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee ${LST} |& -a .log
+fi
+echo "${SECTION} complete"
+
+
+SECTION="Helm"
+addToLogDt "Checking ${SECTION} keyring" y
+
+SRC_URL="https://baltocdn.com/helm/signing.asc"
+KEYRING="/usr/share/keyrings/kubernetes-archive-keyring.gpg"
+ST_FILE="staging/helm_signing.asc"
+LST="/etc/apt/sources.list.d/helm-stable-debian.list"
+
+# import Kubernetes key
+if [ -e ${KEYRING} ]; then
+  addToLogDt "-${SECTION} keyring exists" y
+else
+  addToLogDt "-${SECTION} keyring missing" y
+  curl -fsSL ${SRC_URL} -o ${ST_FILE} |& tee -a .log
+  apt-key add ${ST_FILE}
+  if ! [ -e ${KEYRING} ]; then
+    echo "-Unable to pull ${SECTION} keyring"
+    addToLogDt "-!!! Unable to load ${SECTION} keyring !!!"
+    exit
+  else
+    addToLogDt "-${SECTION} keyring pulled successfully" y
+  fi
+fi
+addToLogDt "Checking ${SECTION} source" y
+if [ -e ${LST} ]; then
+  addToLogDt "-${SECTION} source exists" y
+else
+  addToLogDt "-Adding ${SECTION} source" y
+  echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee ${LST} |& -a .log
+fi
+echo "${SECTION} complete"
 
 
 
 
 
+
+
+
+
+
+# run cleanup to remove staging folder
+cleanUp
 exit 1
 
 
 
-# import kubernetes keys
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 
-# # import helm keys
 # count=$(grep -c "kafkacat" ~/.bash_aliases)
-# curl https://baltocdn.com/helm/signing.asc -o helm_signing.asc & apt-key add helm_signing.asc
-# echo "deb https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
-# rm -f helm_signing.asc
 
 # apt update
 # apt upgrade -y
-
-
-
-# if [ -e /etc/apt/sources.list.d/helm-stable-debian.list ]; then
-# > echo "true"
-# > else
-# > echo false
-# > fi
-# true
-
 
 
 # # Create aliases if not existing
