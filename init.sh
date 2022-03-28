6f6a6aa7d2f2e2fec92d589549da6421a3eaf273
Tyler Laskey <90269666+Tyler-Laskey@users.noreply.github.com>
2022-03-28 14:42:08 -0600

    Fixing HELM

--- init.sh
+++ init.sh
@@ -75,6 +75,7 @@ initKeyrings()
     addToLogDt "-Adding ${SECTION} source" y
     echo "deb [arch=$(dpkg --print-architecture) signed-by=${KEYRING}] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list |& tee -a .log
   fi
+  apt update
   addToLogDt "${SECTION} complete" y
 
 
@@ -108,44 +109,60 @@ initKeyrings()
     addToLogDt "-Adding ${SECTION} source" y
     echo "deb [signed-by=${KEYRING}] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee ${LST} |& tee -a .log
   fi
+  apt update
   addToLogDt "${SECTION} complete" y
 
 
-  SECTION="Helm"
-  addToLogDt "Checking ${SECTION} keyring" y
+  # SECTION="Helm"
+  # addToLogDt "Checking ${SECTION} keyring" y
 
-  SRC_URL="https://baltocdn.com/helm/signing.asc"
-  KEYRING="/usr/share/keyrings/kubernetes-archive-keyring.gpg"
-  ST_FILE="staging/helm_signing.asc"
-  LST="/etc/apt/sources.list.d/helm-stable-debian.list"
-  if [ -e ${KEYRING} ]; then
-    addToLogDt "-${SECTION} keyring exists" y
-  else
-    addToLogDt "-${SECTION} keyring missing" y
-    curl -fsSL ${SRC_URL} -o ${ST_FILE} |& tee -a .log
-    apt-key add ${ST_FILE}
-    if ! [ -e ${KEYRING} ]; then
-      echo "-Unable to pull ${SECTION} keyring"
-      addToLogDt "-!!! Unable to load ${SECTION} keyring !!!"
-      exit
-    else
-      addToLogDt "-${SECTION} keyring pulled successfully" y
-    fi
-  fi
-  addToLogDt "Checking ${SECTION} source" y
-  if [ -e ${LST} ]; then
-    addToLogDt "-${SECTION} source exists" y
-  else
-    addToLogDt "-Adding ${SECTION} source" y
-    echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee ${LST} |& tee -a .log
-  fi
-  addToLogDt "${SECTION} complete" y
+  # SRC_URL="https://baltocdn.com/helm/signing.asc"
+  # KEYRING="/usr/share/keyrings/kubernetes-archive-keyring.gpg"
+  # ST_FILE="staging/helm_signing.asc"
+  # LST="/etc/apt/sources.list.d/helm-stable-debian.list"
+  # if [ -e ${KEYRING} ]; then
+  #   addToLogDt "-${SECTION} keyring exists" y
+  # else
+  #   addToLogDt "-${SECTION} keyring missing" y
+  #   curl -fsSL ${SRC_URL} -o ${ST_FILE} |& tee -a .log
+  #   apt-key add ${ST_FILE}
+  #   if ! [ -e ${KEYRING} ]; then
+  #     echo "-Unable to pull ${SECTION} keyring"
+  #     addToLogDt "-!!! Unable to load ${SECTION} keyring !!!"
+  #     exit
+  #   else
+  #     addToLogDt "-${SECTION} keyring pulled successfully" y
+  #   fi
+  # fi
+  # addToLogDt "Checking ${SECTION} source" y
+  # if [ -e ${LST} ]; then
+  #   addToLogDt "-${SECTION} source exists" y
+  # else
+  #   addToLogDt "-Adding ${SECTION} source" y
+  #   echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee ${LST} |& tee -a .log
+  # fi
+  # addToLogDt "${SECTION} complete" y
 }
 
 installPackages()
 {
   addToLogDt "Installing required apps" y
+  apt update
+  apt upgrade
   apt install -y apt-transport-https ca-certificates conntrack docker-ce docker-ce-cli containerd containerd.io python3 python3-pip python3-venv kafkacat jq unzip libxcb1-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev ca-certificates curl gnupg lsb-release apt-transport-https kubectl helm 
+}
+
+installHelm()
+{
+  addToLogDt "Installing helm" y
+  mkdir -p ~/clarity_init/staging
+  cd ~/clarity_init/staging
+  sudo rm -r helm*
+  wget https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
+  tar xvf helm-v3.4.1-linux-amd64.tar.gz
+  mv linux-amd64/helm /usr/local/bin
+  rm helm-v3.4.1-linux-amd64.tar.gz
+  rm -rf linux-amd64  
 }
 
 installMinikube()
@@ -174,6 +191,7 @@ initSystemd
 initKeyrings
 
 installPackages
+installHelm
 installMinikube
 
 
