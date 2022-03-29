aeb95dad44f1e36ddaeb8d42d57cb669f0fde029
Tyler Laskey <tyler.laskey@telus.com>
2022-03-29 10:57:55 -0600

    more typos in the aliases

--- addAliases.sh
+++ addAliases.sh
@@ -8,15 +8,15 @@ touch ~/.bash_aliases
 #   echo "alias ...='cd ../..'" >> ~/.bash_aliases
 # fi
 
-if [ -z ${cat ~/.bash_aliases | grep " kcat"} ]; then
+if [ -z $(cat ~/.bash_aliases | grep " kcat") ]; then
   echo "alias kcat='kafkacat'" >> ~/.bash_aliases
 fi
 
-if [ -z ${cat ~/.bash_aliases | grep " psx"} ]; then
+if [ -z $(cat ~/.bash_aliases | grep " psx") ]; then
   echo "alias psx='ps aux | grep -v grep'" >> ~/.bash_aliases
 fi
 
-if [ -z ${cat ~/.bash_aliases | grep " py3"} ]; then
+if [ -z $(cat ~/.bash_aliases | grep " py3") ]; then
   echo "alias py3='python3'" >> ~/.bash_aliases
 fi
 
